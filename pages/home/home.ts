0606333e79eebf52193fe3cc0328c82c197f7629
ChanakaWeerasinghe <chanakaweerasinghe92@gmail.com>
2019-11-15 13:56:20 +0530

    2 files modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -1,50 +1,90 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import {Data} from './home.service';
-import { FormBuilder, FormGroup, Validators ,FormControl} from '@angular/forms';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { Data } from "./home.service";
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl
+} from "@angular/forms";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
-  billInquiry:boolean;
-  quickPay : boolean;
-  searchTerm : any="";
-  jsonData : any;
+  billInquiry: boolean=true;
+  quickPay: boolean=false;
+  searchTerm: any = "";
+  jsonData: any;
+
+  idType;
+  cprNumber;
+  crNumber;
   registerForm: FormGroup;
-  constructor(private formBuilder: FormBuilder,public navCtrl: NavController, public data : Data) {
-  
-
-  this.registerForm = this.formBuilder.group({
-            firstName: ['', Validators.required],
-            billInquiry: ['', [Validators.required, Validators.email]],
-            quickPay: ['', [Validators.required, Validators.minLength(6)]],
-            telNumber: ['', Validators.required]
-        }, {
-            
-        });
+  constructor(
+    private formBuilder: FormBuilder,
+    public navCtrl: NavController,
+    public data: Data
+  ) {
+   }
+  ngOnInit() {
+this.initializeForm();
   }
-
-  ionViewDidLoad(){
+  ionViewDidLoad() {
     this.setFilteredItems();
   }
 
   setFilteredItems() {
- 
-        this.jsonData = this.data.filterItems(this.searchTerm);
- 
-    }
-  
-    someEvent(event, type) {
-        console.log(event)
-        if (type == "quickPay") {
-            this.billInquiry = false
-            this.quickPay = true
-        } else if (type == "billInquiry") {
-            this.billInquiry = true
-            this.quickPay = false
-        }
-    }
+    this.jsonData = this.data.filterItems(this.searchTerm);
+  }
 
+  someEvent(event, type) {
+    console.log(type);
+    if (type == "quickPay") {
+      this.billInquiry = false;
+      this.quickPay = true;
+    } else if (type == "billInquiry") {
+      this.billInquiry = true;
+     this.quickPay = false;
+    }
+  }
+
+  initializeForm() {
+    console.log("quickPay",this.quickPay);
+     console.log("billInquiry",this.billInquiry)
+
+    if (this.quickPay) {
+      this.registerForm = this.formBuilder.group(
+        {
+          firstName: ["", Validators.required],
+          billInquiry: ["", [Validators.required, Validators.email]],
+          quickPay: ["", [Validators.required ]],
+          telNumber: ["", Validators.required]
+        },
+        {}
+      );
+    } else {
+      this.registerForm = this.formBuilder.group(
+        {
+          firstName: ["", Validators.required],
+          billInquiry: ["", [Validators.required, Validators.email]],
+          quickPay: ["", [Validators.required, Validators.minLength(6)]],
+          telNumber: ["", Validators.required]
+        },
+        {}
+      );
+    }
+  }
+
+  cprEvent(event, type) {
+    // console.log(event);
+    if (type == "cr") {
+      (this.idType = "cr"), (this.cprNumber = false);
+      this.crNumber = true;
+    } else if (type == "cpr") {
+      (this.idType = "cpr"), (this.cprNumber = true);
+      this.crNumber = false;
+    }
+  }
 }
